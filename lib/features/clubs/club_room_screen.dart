a64b89f0969a83dd2626c6e37ccca0b9a70a2db4
Boolean <danielwnewville@gmail.com>
2021-05-13 17:40:07 -0500

    stop hiding navigation bar in club screen

--- lib/features/clubs/club_room_screen.dart
+++ lib/features/clubs/club_room_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:shodana_reader/core/providers/bottom_navigation_provider.dart';
 
 class ClubRoomScreen extends StatelessWidget {
   const ClubRoomScreen({Key? key, required this.club}) : super(key: key);
@@ -8,19 +6,13 @@ class ClubRoomScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        context.read(shouldShowBottomNavigationProvider).state = true;
-        return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(club['name']!),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('Last Message: ${club['last_message']}'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(club['name']!),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text('Last Message: ${club['last_message']}'),
       ),
     );
   }
--- lib/features/clubs/clubs_screen.dart
+++ lib/features/clubs/clubs_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:beamer/beamer.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:shodana_reader/core/providers/bottom_navigation_provider.dart';
 
 class ClubsScreen extends StatelessWidget {
   const ClubsScreen({Key? key}) : super(key: key);
@@ -15,7 +13,6 @@ class ClubsScreen extends StatelessWidget {
       body: Center(
         child: TextButton(
           onPressed: () {
-            context.read(shouldShowBottomNavigationProvider).state = false;
             context.beamToNamed('/clubs/0}');
           },
           child: const Text('Beam to Test Club 0'),
