1f27a04798796c2a6bfbac25eb6f754aed649d3b
Boolean <danielwnewville@gmail.com>
2021-05-13 16:31:42 -0500

    Create fake_data.dart

---
+++ lib/core/data/fake_data.dart
@@ -0,0 +1,23 @@
+Map<String, String> book = {'title': 'New Book', 'author': 'Best Author',
+  'id': '0'};
+
+Map<String, String> shelf = {'name': 'Fav Series', 'author': 'Best Author',
+  'id': '0'};
+
+Map<String, String> club = {
+  'name': 'Tea Time',
+  'last_message': 'wow that was really cool',
+  'id': '0'
+};
+
+Map<String, String> option = {
+  'name': 'Settings',
+  'description': 'various settings to change app behavior',
+  'id': '0'
+};
+
+Map<String, String> internetBook = {
+  'title': 'New Book',
+  'author': 'Best Author',
+  'id': '0'
+};
