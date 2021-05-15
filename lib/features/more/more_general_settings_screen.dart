813b2c1d309435b3d2fea8b071f94f8cf3018a59
Boolean <danielwnewville@gmail.com>
2021-05-14 22:58:36 -0500

    Just use SettingsTile in a column instead of SettingsList

--- lib/features/more/more_general_settings_screen.dart
+++ lib/features/more/more_general_settings_screen.dart
@@ -13,57 +13,30 @@ class GeneralSettings extends HookWidget {
         title: Text(AppLocalizations.of(context)?.generalPageTitle
             ?? 'No Title'),
       ),
-      body: SettingsList(
-        backgroundColor: Colors.white,
-        sections: [
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           SettingsSection(
             title: '\nTheme',
-            tiles: [
-              SettingsTile(
-                title: 'Dark mode',
-                subtitle: 'Follow system',
-                onPressed: (BuildContext context) {},
-              ),
-              SettingsTile(
-                title: 'Light theme',
-                subtitle: 'Default',
-                onPressed: (BuildContext context) {},
-              ),
-              SettingsTile(
-                title: 'Dark theme',
-                subtitle: 'Default',
-                onPressed: (BuildContext context) {},
-              ),
-            ],
+            tiles: const [],
+          ),
+          SettingsTile(
+            title: 'Dark mode',
+            subtitle: 'Follow system',
+            onPressed: (BuildContext context) {},
+          ),
+          SettingsTile(
+            title: 'Light theme',
+            subtitle: 'Default',
+            onPressed: (BuildContext context) {},
+          ),
+          SettingsTile(
+            title: 'Dark theme',
+            subtitle: 'Default',
+            onPressed: (BuildContext context) {},
           ),
         ],
       ),
-      // body: Column(
-      //   crossAxisAlignment: CrossAxisAlignment.start,
-      //   children: [
-      //     const SizedBox(height: 8.0,),
-      //     const Divider(),
-      //     SettingsItemButton(
-      //         key: const ValueKey('dark_mode'),
-      //         text: 'Dark mode',
-      //         secondaryText: 'Follow system',
-      //         onPressed: showDarkModeDialog
-      //     ),
-      //     SettingsItemButton(
-      //         key: const ValueKey('light_theme'),
-      //         text: 'Light theme',
-      //         secondaryText: 'Default',
-      //         onPressed: () {}
-      //     ),
-      //     SettingsItemButton(
-      //         key: const ValueKey('dark_theme'),
-      //         text: 'Dark theme',
-      //         secondaryText: 'Default',
-      //         onPressed: () {}
-      //     ),
-      //     const Divider(),
-      //   ],
-      // ),
     );
   }
 
