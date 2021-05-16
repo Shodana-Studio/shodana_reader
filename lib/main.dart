62d4842fef57ec8c96095f0092b9f3f3fdebf9ff
Boolean <danielwnewville@gmail.com>
2021-05-16 02:29:44 -0500

    Set system to dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_strategy/url_strategy.dart';
 
@@ -9,6 +10,9 @@ import 'data/local/storage_utils.dart';
 Future<void> main() async {
   setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.light,
+  );
   await StorageUtil.getInstance();
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
   runApp(ProviderScope(child: App(savedThemeMode: savedThemeMode)));
