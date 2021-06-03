0da93051d53f8c5ea4a59b3372e68fc63b5605e9
Boolean <danielwnewville@gmail.com>
2021-06-03 16:40:29 -0500

    fix: Fixed set dark theme button not changing navigation bar color

--- lib/ui/more/more_general_settings/more_general_settings_list.dart
+++ lib/ui/more/more_general_settings/more_general_settings_list.dart
@@ -1,5 +1,6 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -27,7 +28,10 @@ class GeneralSettingsList extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // Starting Screen
           const StartingScreenWidget(),
+
+          // Left handed navigation rail
           SwitchListTile(
             title: Text(AppLocalizations.of(context)!.leftHandedNavigationRailSwitchText),
             // subtitle: Text(leftRail ? AppLocalizations.of(context)!
@@ -39,10 +43,15 @@ class GeneralSettingsList extends StatelessWidget {
             },
             value: leftRail,
           ),
+
+
+          // Theme section
           const Divider(height: 1.0),
           SettingsSectionHeader(
             title: AppLocalizations.of(context)!.themeSectionText,
           ),
+
+          // Dark mode toggle
           SwitchListTile(
             title: Text(AppLocalizations.of(context)!.darkModeSwitchText),
             onChanged: (bool value) {
@@ -50,26 +59,96 @@ class GeneralSettingsList extends StatelessWidget {
               if (!followSysTheme) {
                 if (value) {
                   AdaptiveTheme.of(context).setDark();
+                  SystemChrome.setSystemUIOverlayStyle(
+                    SystemUiOverlayStyle(
+                      systemNavigationBarColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                      systemNavigationBarDividerColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                      systemNavigationBarIconBrightness: Brightness.light,
+                      statusBarColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                      statusBarBrightness: Brightness.dark,
+                      statusBarIconBrightness: Brightness.light,
+                    ),
+                  );
                 } else {
                   AdaptiveTheme.of(context).setLight();
+                  SystemChrome.setSystemUIOverlayStyle(
+                    SystemUiOverlayStyle(
+                      systemNavigationBarColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                      systemNavigationBarDividerColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                      systemNavigationBarIconBrightness: Brightness.dark,
+                      statusBarColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                      statusBarBrightness: Brightness.light,
+                      statusBarIconBrightness: Brightness.dark,
+                    ),
+                  );
                 }
               }
             },
             value: darkMode,
           ),
+          
+          // Follow systen theme switch
           SwitchListTile(
             title: Text(AppLocalizations.of(context)!.followSysThemeSwitchText),
             subtitle: Text(AppLocalizations.of(context)!.followSysThemeSwitchSubtitle),
             onChanged: (bool value) {
               context.read(followSystemThemeSwitchProvider.notifier).toggle();
               if (value) {
+                // final backgroundColor = Theme.of(context).backgroundColor;
                 AdaptiveTheme.of(context).setSystem();
+                // print(backgroundColor.toString());
+                // print(AdaptiveTheme.of(context).lightTheme.backgroundColor.toString());
+                // if (backgroundColor == AdaptiveTheme.of(context).lightTheme.backgroundColor) {
+                //   SystemChrome.setSystemUIOverlayStyle(
+                //     SystemUiOverlayStyle(
+                //       systemNavigationBarColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                //       systemNavigationBarDividerColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                //       systemNavigationBarIconBrightness: Brightness.dark,
+
+                //       statusBarColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                //       statusBarBrightness: Brightness.light,
+                //       statusBarIconBrightness: Brightness.dark,
+                //     ),
+                //   );
+                // } else {
+                //   SystemChrome.setSystemUIOverlayStyle(
+                //     SystemUiOverlayStyle(
+                //       systemNavigationBarColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                //       systemNavigationBarDividerColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                //       systemNavigationBarIconBrightness: Brightness.light,
+
+                //       statusBarColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                //       statusBarBrightness: Brightness.dark,
+                //       statusBarIconBrightness: Brightness.light,
+                //     ),
+                //   );
+                // }
               }
               else {
                 if (darkMode) {
                   AdaptiveTheme.of(context).setDark();
+                  SystemChrome.setSystemUIOverlayStyle(
+                    SystemUiOverlayStyle(
+                      systemNavigationBarColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                      systemNavigationBarDividerColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                      systemNavigationBarIconBrightness: Brightness.light,
+                      statusBarColor: AdaptiveTheme.of(context).darkTheme.backgroundColor,
+                      statusBarBrightness: Brightness.dark,
+                      statusBarIconBrightness: Brightness.light,
+                    ),
+                  );
                 } else {
                   AdaptiveTheme.of(context).setLight();
+                  SystemChrome.setSystemUIOverlayStyle(
+                    SystemUiOverlayStyle(
+                      systemNavigationBarColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                      systemNavigationBarDividerColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                      systemNavigationBarIconBrightness: Brightness.dark,
+                      statusBarColor: AdaptiveTheme.of(context).lightTheme.backgroundColor,
+                      statusBarBrightness: Brightness.light,
+                      statusBarIconBrightness: Brightness.dark,
+                    ),
+                  );
                 }
               }
             },
