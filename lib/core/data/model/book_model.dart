213b0851c18dc5799e89d0e01cc867cb313eba9d
Boolean <danielwnewville@gmail.com>
2021-06-04 01:01:47 -0500

    feat: Book model now has fromMap, toMap, fromJson, toJson, copyWith, and toString

--- lib/core/data/model/book_model.dart
+++ lib/core/data/model/book_model.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 
 import 'file_model.dart';
-@immutable
+
 class BookModel extends FileModel {
   const BookModel({
     required String title,
@@ -12,16 +12,27 @@ class BookModel extends FileModel {
         _author = author,
         super(filename: filename, path: path);
 
-  factory BookModel.fromJson(Map<String, dynamic> map) {
-    final Map<String, dynamic> props = map['properties'];
-
+  factory BookModel.fromMap(Map<String, dynamic> map) {
     return BookModel(
-      title: props['title'],
-      author: props['author'],
-      filename: props['filename'],
-      path: props['path'],
+      title: map['title'],
+      author: map['author'],
+      filename: map['filename'],
+      path: map['path'],
     );
   }
+
+  factory BookModel.fromJson(String source) => BookModel.fromMap(json.decode(source));
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'author': author,
+      'filename': filename,
+      'path': path,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
 
   final String _title;
   final String _author;
@@ -46,4 +57,19 @@ class BookModel extends FileModel {
 
   @override
   List<Object?> get props => [title, author, filename, path];
+
+
+  BookModel copyWith({
+    String? title,
+    String? author,
+    String? filename,
+    String? path,
+  }) {
+    return BookModel(
+      title: title ?? this.title,
+      author: author ?? this.author,
+      filename: filename ?? this.filename,
+      path: path ?? this.path,
+    );
+  }
 }
