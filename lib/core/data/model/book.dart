a8d5995fe9e9ccc58c0af7537cc30c200b604375
getBoolean <danielwnewville@gmail.com>
2021-08-20 21:12:04 -0500

    Update book model to match appwrite database

--- lib/core/data/model/book.dart
+++ lib/core/data/model/book.dart
@@ -5,121 +5,190 @@ import 'package:equatable/equatable.dart';
 import 'reading_stats.dart';
 
 class Book extends Equatable {
+  // Constructors
   const Book({
+    required this.userId,
+    required this.fileId,
     this.title,
+    this.titleLastModDate,
     this.author,
+    this.authorLastModDate,
     this.description,
-    required this.filename,
+    this.descriptionLastModDate,
+    this.readingProgress,
+    this.readingProgressLastModDate,
     this.publisher,
+    this.publisherLastModDate,
     this.publishedDate,
+    this.publishedDateLastModDate,
     required this.createdDate,
     this.startReadingDate,
+    this.startReadingDateLastModDate,
     this.finishReadingDate,
-    this.readingProgress,
-    required this.userId,
+    this.finishReadingDateLastModDate,
     this.metadata,
+    this.metadataLastModDate,
     this.readingTimes,
   });
 
   factory Book.fromMap(Map<String, dynamic> map) {
     return Book(
+      userId: map['userId'],
+      fileId: map['fileId'],
       title: map['title'],
+      titleLastModDate: DateTime.fromMillisecondsSinceEpoch(map['titleLastModDate']),
       author: map['author'],
+      authorLastModDate: DateTime.fromMillisecondsSinceEpoch(map['authorLastModDate']),
       description: map['description'],
-      filename: map['filename'],
+      descriptionLastModDate: DateTime.fromMillisecondsSinceEpoch(map['descriptionLastModDate']),
+      readingProgress: map['readingProgress'],
+      readingProgressLastModDate: DateTime.fromMillisecondsSinceEpoch(map['readingProgressLastModDate']),
       publisher: map['publisher'],
+      publisherLastModDate: DateTime.fromMillisecondsSinceEpoch(map['publisherLastModDate']),
       publishedDate: DateTime.fromMillisecondsSinceEpoch(map['publishedDate']),
+      publishedDateLastModDate: DateTime.fromMillisecondsSinceEpoch(map['publishedDateLastModDate']),
       createdDate: DateTime.fromMillisecondsSinceEpoch(map['createdDate']),
       startReadingDate: DateTime.fromMillisecondsSinceEpoch(map['startReadingDate']),
+      startReadingDateLastModDate: DateTime.fromMillisecondsSinceEpoch(map['startReadingDateLastModDate']),
       finishReadingDate: DateTime.fromMillisecondsSinceEpoch(map['finishReadingDate']),
-      readingProgress: map['readingProgress'],
-      userId: map['userId'],
+      finishReadingDateLastModDate: DateTime.fromMillisecondsSinceEpoch(map['finishReadingDateLastModDate']),
       metadata: map['metadata'],
+      metadataLastModDate: DateTime.fromMillisecondsSinceEpoch(map['metadataLastModDate']),
       readingTimes: List<ReadingStats>.from((map['readingTimes'] as List<Map<String, ReadingStats>>).map((x) => ReadingStats.fromMap(x))),
     );
   }
 
   factory Book.fromJson(String source) => Book.fromMap(json.decode(source));
+  
+  // Variables
+  final String userId;
+  final String fileId;
 
   final String? title;
+  final DateTime? titleLastModDate;
+
   final String? author;
+  final DateTime? authorLastModDate;
+
   final String? description;
-  final String filename;
-  final String? publisher;
-  final DateTime? publishedDate;
-  final DateTime createdDate;
-  final DateTime? startReadingDate;
-  final DateTime? finishReadingDate;
+  final DateTime? descriptionLastModDate;
+
   final int? readingProgress;
-  final String userId;
+  final DateTime? readingProgressLastModDate;
+
+  final String? publisher;
+  final DateTime? publisherLastModDate;
+
+  final DateTime? publishedDate;
+  final DateTime? publishedDateLastModDate;
+
+  final DateTime createdDate;
+
+  final DateTime? startReadingDate;
+  final DateTime? startReadingDateLastModDate;
+
+  final DateTime? finishReadingDate;
+  final DateTime? finishReadingDateLastModDate;
+
   final dynamic metadata;
+  final DateTime? metadataLastModDate;
+
   final List<ReadingStats>? readingTimes;
 
+  // Utility Functions
   Map<String, dynamic> toMap() {
     return {
+      'userId': userId,
+      'fileId': fileId,
       'title': title,
+      'titleLastModDate': titleLastModDate?.millisecondsSinceEpoch,
       'author': author,
+      'authorLastModDate': authorLastModDate?.millisecondsSinceEpoch,
       'description': description,
-      'filename': filename,
+      'descriptionLastModDate': descriptionLastModDate?.millisecondsSinceEpoch,
+      'readingProgress': readingProgress,
+      'readingProgressLastModDate': readingProgressLastModDate?.millisecondsSinceEpoch,
       'publisher': publisher,
+      'publisherLastModDate': publisherLastModDate?.millisecondsSinceEpoch,
       'publishedDate': publishedDate?.millisecondsSinceEpoch,
+      'publishedDateLastModDate': publishedDateLastModDate?.millisecondsSinceEpoch,
       'createdDate': createdDate.millisecondsSinceEpoch,
       'startReadingDate': startReadingDate?.millisecondsSinceEpoch,
+      'startReadingDateLastModDate': startReadingDateLastModDate?.millisecondsSinceEpoch,
       'finishReadingDate': finishReadingDate?.millisecondsSinceEpoch,
-      'readingProgress': readingProgress,
-      'userId': userId,
+      'finishReadingDateLastModDate': finishReadingDateLastModDate?.millisecondsSinceEpoch,
       'metadata': metadata,
+      'metadataLastModDate': metadataLastModDate?.millisecondsSinceEpoch,
       'readingTimes': readingTimes?.map((x) => x.toMap()).toList(),
     };
   }
 
-  String toJson() => json.encode(toMap());
-
   Book copyWith({
+    String? userId,
+    String? fileId,
     String? title,
+    DateTime? titleLastModDate,
     String? author,
+    DateTime? authorLastModDate,
     String? description,
-    String? filename,
+    DateTime? descriptionLastModDate,
+    int? readingProgress,
+    DateTime? readingProgressLastModDate,
     String? publisher,
+    DateTime? publisherLastModDate,
     DateTime? publishedDate,
+    DateTime? publishedDateLastModDate,
     DateTime? createdDate,
     DateTime? startReadingDate,
+    DateTime? startReadingDateLastModDate,
     DateTime? finishReadingDate,
-    int? readingProgress,
-    String? userId,
+    DateTime? finishReadingDateLastModDate,
     dynamic metadata,
+    DateTime? metadataLastModDate,
     List<ReadingStats>? readingTimes,
   }) {
     return Book(
+      userId: userId ?? this.userId,
+      fileId: fileId ?? this.fileId,
       title: title ?? this.title,
+      titleLastModDate: titleLastModDate ?? this.titleLastModDate,
       author: author ?? this.author,
+      authorLastModDate: authorLastModDate ?? this.authorLastModDate,
       description: description ?? this.description,
-      filename: filename ?? this.filename,
+      descriptionLastModDate: descriptionLastModDate ?? this.descriptionLastModDate,
+      readingProgress: readingProgress ?? this.readingProgress,
+      readingProgressLastModDate: readingProgressLastModDate ?? this.readingProgressLastModDate,
       publisher: publisher ?? this.publisher,
+      publisherLastModDate: publisherLastModDate ?? this.publisherLastModDate,
       publishedDate: publishedDate ?? this.publishedDate,
+      publishedDateLastModDate: publishedDateLastModDate ?? this.publishedDateLastModDate,
       createdDate: createdDate ?? this.createdDate,
       startReadingDate: startReadingDate ?? this.startReadingDate,
+      startReadingDateLastModDate: startReadingDateLastModDate ?? this.startReadingDateLastModDate,
       finishReadingDate: finishReadingDate ?? this.finishReadingDate,
-      readingProgress: readingProgress ?? this.readingProgress,
-      userId: userId ?? this.userId,
+      finishReadingDateLastModDate: finishReadingDateLastModDate ?? this.finishReadingDateLastModDate,
       metadata: metadata ?? this.metadata,
+      metadataLastModDate: metadataLastModDate ?? this.metadataLastModDate,
       readingTimes: readingTimes ?? this.readingTimes,
     );
   }
 
-  @override
-  List<Object> get props {
-    return [
-      title ?? '',
-      author ?? '',
-      description ?? '',
-      filename,
-      readingProgress ?? -1,
-      userId,
-      publisher ?? '',
-    ];
-  }
+  String toJson() => json.encode(toMap());
 
   @override
   bool get stringify => true;
+
+  @override
+  List<Object> get props {
+    return [
+      userId,
+      fileId,
+      title ?? '',
+      author ?? '',
+      description ?? '',
+      readingProgress ?? 0,
+      publisher ?? '',
+      createdDate,
+    ];
+  }
 }
