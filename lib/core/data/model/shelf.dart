a73a95b5d6093c9b3c270c5051fb8ef8cfba9b5d
getBoolean <danielwnewville@gmail.com>
2021-08-20 21:30:00 -0500

    Update shelf model to match appwrite database

--- lib/core/data/model/shelf.dart
+++ lib/core/data/model/shelf.dart
@@ -5,69 +5,116 @@ import 'package:equatable/equatable.dart';
 import 'book.dart';
 
 class Shelf extends Equatable {
+  // Constructors
   const Shelf({
-    required this.title,
-    this.description,
-    required this.books,
     required this.userId,
+    required this.title,
+    required this.titleLastModDate,
     required this.createdDate,
-    this.lastModifiedDate, 
+    this.description,
+    this.descriptionLastModDate,
+    this.startReadingDate,
+    this.startReadingDateLastModDate,
+    this.finishReadingDate,
+    this.finishReadingDateLastModDate,
+    this.books,
   });
-  
+
   factory Shelf.fromMap(Map<String, dynamic> map) {
     return Shelf(
-      title: map['title'],
-      description: map['description'],
-      books: List<Book>.from((map['books'] as List<Map<String, Book>>).map((x) => Book.fromMap(x))),
-      createdDate: DateTime.fromMillisecondsSinceEpoch(map['createdDate']),
-      lastModifiedDate: DateTime.fromMillisecondsSinceEpoch(map['lastModifiedDate']),
       userId: map['userId'],
+      title: map['title'],
+      titleLastModDate: DateTime.fromMillisecondsSinceEpoch(map['titleLastModDate']),
+      createdDate: DateTime.fromMillisecondsSinceEpoch(map['createdDate']),
+      description: map['description'],
+      descriptionLastModDate: DateTime.fromMillisecondsSinceEpoch(map['descriptionLastModDate']),
+      startReadingDate: DateTime.fromMillisecondsSinceEpoch(map['startReadingDate']),
+      startReadingDateLastModDate: DateTime.fromMillisecondsSinceEpoch(map['startReadingDateLastModDate']),
+      finishReadingDate: DateTime.fromMillisecondsSinceEpoch(map['finishReadingDate']),
+      finishReadingDateLastModDate: DateTime.fromMillisecondsSinceEpoch(map['finishReadingDateLastModDate']),
+      books: List<Book>.from((map['books'] as List<Map<String, Book>>).map((x) => Book.fromMap(x))),
     );
   }
 
   factory Shelf.fromJson(String source) => Shelf.fromMap(json.decode(source));
 
-  final String title;
-  final String? description;
-  final List<Book> books;
+  // Variables
   final String userId;
-  final DateTime createdDate;
-  final DateTime? lastModifiedDate;
 
+  final String title;
+  final DateTime titleLastModDate;
+
+  final DateTime createdDate;
+
+  final String? description;
+  final DateTime? descriptionLastModDate;
+
+  final DateTime? startReadingDate;
+  final DateTime? startReadingDateLastModDate;
+  
+  final DateTime? finishReadingDate;
+  final DateTime? finishReadingDateLastModDate;
+
+  final List<Book>? books;
+
+  // Utility Functions
   Shelf copyWith({
-    String? title,
-    String? description,
-    List<Book>? books,
     String? userId,
+    String? title,
+    DateTime? titleLastModDate,
     DateTime? createdDate,
-    DateTime? lastModifiedDate,
+    String? description,
+    DateTime? descriptionLastModDate,
+    DateTime? startReadingDate,
+    DateTime? startReadingDateLastModDate,
+    DateTime? finishReadingDate,
+    DateTime? finishReadingDateLastModDate,
+    List<Book>? books,
   }) {
     return Shelf(
-      title: title ?? this.title,
-      description: description ?? this.description,
-      books: books ?? this.books,
       userId: userId ?? this.userId,
+      title: title ?? this.title,
+      titleLastModDate: titleLastModDate ?? this.titleLastModDate,
       createdDate: createdDate ?? this.createdDate,
-      lastModifiedDate: lastModifiedDate ?? this.lastModifiedDate,
+      description: description ?? this.description,
+      descriptionLastModDate: descriptionLastModDate ?? this.descriptionLastModDate,
+      startReadingDate: startReadingDate ?? this.startReadingDate,
+      startReadingDateLastModDate: startReadingDateLastModDate ?? this.startReadingDateLastModDate,
+      finishReadingDate: finishReadingDate ?? this.finishReadingDate,
+      finishReadingDateLastModDate: finishReadingDateLastModDate ?? this.finishReadingDateLastModDate,
+      books: books ?? this.books,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
-      'title': title,
-      'description': description,
-      'books': books.map((x) => x.toMap()).toList(),
       'userId': userId,
+      'title': title,
+      'titleLastModDate': titleLastModDate.millisecondsSinceEpoch,
       'createdDate': createdDate.millisecondsSinceEpoch,
-      'lastModifiedDate': createdDate.millisecondsSinceEpoch,
+      'description': description,
+      'descriptionLastModDate': descriptionLastModDate?.millisecondsSinceEpoch,
+      'startReadingDate': startReadingDate?.millisecondsSinceEpoch,
+      'startReadingDateLastModDate': startReadingDateLastModDate?.millisecondsSinceEpoch,
+      'finishReadingDate': finishReadingDate?.millisecondsSinceEpoch,
+      'finishReadingDateLastModDate': finishReadingDateLastModDate?.millisecondsSinceEpoch,
+      'books': books?.map((x) => x.toMap()).toList(),
     };
   }
 
   String toJson() => json.encode(toMap());
 
+  // Overrides
   @override
   bool get stringify => true;
 
   @override
-  List<Object> get props => [title, description ?? '', createdDate];
+  List<Object> get props {
+    return [
+      userId,
+      title,
+      // Allows shelves with the same title
+      createdDate,
+    ];
+  }
 }
