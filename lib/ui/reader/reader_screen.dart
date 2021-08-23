2a069a839b5cf2f05ec5ecff492665de24b6da51
getBoolean <danielwnewville@gmail.com>
2021-08-23 16:15:20 -0500

    Add temp shortcuts to reader screen

--- lib/ui/reader/reader_screen.dart
+++ lib/ui/reader/reader_screen.dart
@@ -7,7 +7,7 @@ import '../app_screen/provider/bottom_navigation_provider.dart';
 import 'ebook_reader/ebook_reader_screen.dart';
 import 'html_reader/html_reader_screen.dart';
 
-class ReaderScreen extends StatelessWidget {
+class ReaderScreen extends StatefulWidget {
   const ReaderScreen({
     Key? key,
     required this.book,
@@ -17,7 +17,14 @@ class ReaderScreen extends StatelessWidget {
   final BookFileType bookFileType;
   final Map<String, String> book;
   final List<String> chapter;
-  // Handle each type of book differently
+
+  @override
+  _ReaderScreenState createState() => _ReaderScreenState();
+}
+
+class _ReaderScreenState extends State<ReaderScreen> {
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
@@ -27,18 +34,52 @@ class ReaderScreen extends StatelessWidget {
         await SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
         return true;
       },
-      child: Padding(
-        padding: const EdgeInsets.only(top: 24.0),
-        child: bookFileType == BookFileType.epub ?
-          EBookReaderScreen(
-            book: book,
-            chapter: chapter
-          ) :
-          HTMLReaderScreen(
-            book: book,
-            chapter: chapter,
+      child: Shortcuts(
+        shortcuts: <LogicalKeySet, Intent>{
+          LogicalKeySet(LogicalKeyboardKey.arrowUp) : const IncrementIntent(),
+          LogicalKeySet(LogicalKeyboardKey.arrowDown) : const DecrementIntent(),
+        },
+        child: Actions(
+          actions: <Type, Action<Intent>>{
+            // TODO: Change these to control page scrolling
+            IncrementIntent: CallbackAction<IncrementIntent>(
+                onInvoke: (IncrementIntent intent) => setState(() {
+                  count = count + 1;
+                  print(count);
+                })
+            ),
+            DecrementIntent: CallbackAction<DecrementIntent>(
+                onInvoke: (DecrementIntent intent) => setState(() {
+                  count = count - 1;
+                  print(count);
+                })
+            ),
+          },
+          child: Focus(
+            autofocus: true,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 24.0),
+              child: widget.bookFileType == BookFileType.epub ?
+              EBookReaderScreen(
+                  book: widget.book,
+                  chapter: widget.chapter
+              ) :
+              HTMLReaderScreen(
+                book: widget.book,
+                chapter: widget.chapter,
+              ),
+            ),
           ),
-      ),
+        ),
+      )
     );
   }
+}
+
+class IncrementIntent extends Intent {
+  const IncrementIntent();
+}
+
+class DecrementIntent extends Intent {
+  const DecrementIntent();
 }
