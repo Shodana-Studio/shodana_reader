8969f91f6ec6e95f0795fb244061207367ceb531
getBoolean <danielwnewville@gmail.com>
2021-08-26 13:13:58 -0500

    refactor: make directory final

--- lib/core/data/model/book.dart
+++ lib/core/data/model/book.dart
@@ -243,14 +243,14 @@ class Book extends Equatable {
   /// Gets the path to the epub file.
   /// Ex: Documents/ShodanaReader/123456789/123456789
   Future<String> get bookPath async {
-    io.Directory dir = await StorageUtil.getAppDirectory();
+    final io.Directory dir = await StorageUtil.getAppDirectory();
     return p.join(dir.path, 'ShodanaReader', bookId, bookId);
   }
 
   /// Gets the path to the directory the epub and cover image is in.
   /// Ex: Documents/ShodanaReader/123456789
   Future<String> get bookDirectoryPath async {
-    io.Directory dir = await StorageUtil.getAppDirectory();
+    final io.Directory dir = await StorageUtil.getAppDirectory();
     return p.join(dir.path, 'ShodanaReader', bookId);
   }
 
