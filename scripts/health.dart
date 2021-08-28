83e28848070ce9435b93704a83e088b81f7c17f1
getBoolean <danielwnewville@gmail.com>
2021-08-28 14:06:00 -0500

    Revert "doc: make easier to copy"
    This reverts commit b2507bdac308da02327fe014d8beb229fee80cb9.

--- scripts/health.dart
+++ scripts/health.dart
@@ -2,11 +2,10 @@
 // project id, and an api key with access to health read
 // 
 // Run the following all on the same line:
-/*
- dart scripts/health.dart --dart-define=appwriteEndpoint={ENDPOINT}
-  --dart-define=appwriteProjectId={PROJECT_ID}
-  --dart-define=appwriteApiKey={KEY}
-*/
+// dart scripts/health.dart --dart-define=appwriteEndpoint={ENDPOINT}
+//  --dart-define=appwriteProjectId={PROJECT_ID}
+//  --dart-define=appwriteApiKey={KEY}
+
 // ignore_for_file: avoid_print
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:shodana_reader/core/res/environment_config.dart';
