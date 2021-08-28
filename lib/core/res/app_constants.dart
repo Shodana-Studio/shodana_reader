bdc3bfc200177427686101c864a4ddb990e8bc54
getBoolean <danielwnewville@gmail.com>
2021-08-27 22:07:51 -0500

    Update appwrite url

--- lib/core/res/app_constants.dart
+++ lib/core/res/app_constants.dart
@@ -28,8 +28,8 @@ class AppConstant {
   // static const String endpoint = 'http://129.107.97.163/v1';
   // static const String project = '60bbe0b4363f5';
   // Desktop
-  static const String url = 'http://192.168.1.191';
-  static const String endpoint = 'http://192.168.1.191/v1';
+  static const String url = 'http://172.24.240.1';
+  static const String endpoint = 'http://172.24.240.1/v1';
   static const String project = '611ffbae6bdd9';
 
   // Appwrite Database
