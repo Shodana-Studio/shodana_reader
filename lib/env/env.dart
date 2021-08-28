406150fec5377ff6443c45c480a32f5ec7238315
getBoolean <danielwnewville@gmail.com>
2021-08-28 15:24:39 -0500

    remove envify

--- lib/env/env.dart
+++
@@ -1,8 +0,0 @@
-import 'package:envify/envify.dart';
-part 'env.g.dart';
-
-@Envify(path: 'scripts/.env', name: 'ScriptEnv')
-abstract class ScriptEnv {
-    static const databaseKey = _ScriptEnv.databaseKey;
-    static const healthKey = _ScriptEnv.healthKey;
-}
