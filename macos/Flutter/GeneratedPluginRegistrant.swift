5e9ca3f37d11bb246c512ce5a7ffe0976a30c6de
getBoolean <danielwnewville@gmail.com>
2021-09-03 16:06:32 -0500

    remove native draggable

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import device_info_plus_macos
 import flutter_web_auth
-import native_draggable
 import package_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
@@ -17,7 +16,6 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterWebAuthPlugin.register(with: registry.registrar(forPlugin: "FlutterWebAuthPlugin"))
-  NativeDraggablePlugin.register(with: registry.registrar(forPlugin: "NativeDraggablePlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
